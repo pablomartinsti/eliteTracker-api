8eed4cf682d3524c97e33973b76a30e957ff72e5
PABLO MARTINS git config --global user.email <seuemail@exemplo.com>
2024-12-03 17:26:59 -0300

    Impedindo a criaçao hábitos duplicado e fazer a validação com o zod

--- src/controller/controller.habits.ts
+++ src/controller/controller.habits.ts
@@ -1,13 +1,32 @@
 import { type Request, type Response } from 'express';
+import { z } from 'zod';
 
 import { habitModel } from '../models/model.habits';
 
 export class HabitsController {
   store = async (request: Request, response: Response): Promise<Response> => {
+    const schema = z.object({
+      name: z.string(),
+    });
     const { name } = request.body;
 
-    const newHabit = await habitModel.create({
+    const habit = schema.safeParse({
       name,
+    });
+
+    if (!habit.success) {
+      return response.status(400).json({ message: 'Error on validation' });
+    }
+    const findHabit = await habitModel.findOne({
+      name: habit.data.name,
+    });
+
+    if (findHabit) {
+      return response.status(400).json({ message: 'Habit already exists' });
+    }
+
+    const newHabit = await habitModel.create({
+      name: habit.data.name,
       completedDate: [],
     });
 
